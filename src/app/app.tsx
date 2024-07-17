363c6055328ea94960fab9c3153c1b297284f64b
Paolo Mosconi <paolo.mosconi@actvalue.com>
2024-07-17 16:05:30 +0200

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="nx-env-test" />
+    </div>
+  );
+}
+
+export default App;
